c05fcac14a92a1b964c1c0270e4ff55aedcedd4e
Zelib <danyailenin@gmail.com>
2022-05-24 19:08:44 +0300

    new design

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const slide = document.querySelector('.product__line');
 let count = 0;
 
 document.querySelector('.product__next').addEventListener('click', function(){
-  count += 550;
-  if (count > 2200) {
+  count += 700;
+  if (count > 2800) {
     count = 0;
   }
   slide.style.left = -count + 'px';
 });
 
 document.querySelector('.product__pre').addEventListener('click', function(){
-  count -= 550;
+  count -= 700;
   if (count < 0) {
-    count = 2200;
+    count = 2800;
   }
   slide.style.left = -count + 'px';
 });
